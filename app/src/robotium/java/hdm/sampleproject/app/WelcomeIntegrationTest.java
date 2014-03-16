b3ca60069d0293abae495ced27f3d4663622edf4
friedolinfoerder <friedolinfoerder@gmx.de>
2014-03-16 16:47:26 +0100

    Update WelcomeIntegrationTest.java

--- app/src/robotium/java/hdm/sampleproject/app/WelcomeIntegrationTest.java
+++ app/src/robotium/java/hdm/sampleproject/app/WelcomeIntegrationTest.java
@@ -7,15 +7,19 @@ import android.util.Log;
 import com.robotium.solo.Solo;
 
 /**
- * Created by schreon on 3/15/14.
+ * @author Leon Schröder
+ * @author Friedolin Förder
+ * WelcomeIntegrationTest
  */
 public class WelcomeIntegrationTest extends ActivityInstrumentationTestCase2<WelcomeActivity> {
-
+    private Solo solo;
+    
+    /**
+     * Constructor
+     */
     public WelcomeIntegrationTest() {
         super(WelcomeActivity.class);
     }
-
-    private Solo solo;
 
     @Override
     protected void setUp() throws Exception {
@@ -24,15 +28,21 @@ public class WelcomeIntegrationTest extends ActivityInstrumentationTestCase2<Wel
         Log.d("robotium", "creating solo");
         solo = new Solo(getInstrumentation(), getActivity());
     }
-
+    
+    /**
+     * Tests if there is a text welcome! available at the WelcomeActivity
+     */ 
     public void testWelcome() throws Exception {
+        Log.d("robotium", "wait for the activity to appear");
         solo.waitForActivity(WelcomeActivity.class);
+        
+        Log.d("robotium", "check if there is the text welcome!");
         assertTrue(solo.searchText("welcome!"));
     }
-
+    
     @Override
     public void tearDown() throws Exception {
-        // finish activity
+        Log.d("robotium", "finish the activity");
         solo.finishOpenedActivities();
 
         super.tearDown();
