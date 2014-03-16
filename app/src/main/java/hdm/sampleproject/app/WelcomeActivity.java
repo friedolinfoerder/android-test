7778c57544d95e6d5bacca6db563def5d173f303
friedolinfoerder <friedolinfoerder@gmx.de>
2014-03-16 16:48:42 +0100

    Update WelcomeActivity.java

--- app/src/main/java/hdm/sampleproject/app/WelcomeActivity.java
+++ app/src/main/java/hdm/sampleproject/app/WelcomeActivity.java
@@ -3,6 +3,11 @@ package hdm.sampleproject.app;
 import android.app.Activity;
 import android.os.Bundle;
 
+/**
+ * @author Leon Schröder
+ * @author Friedolin Förder
+ * WelcomeActivity
+ */
 public class WelcomeActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
